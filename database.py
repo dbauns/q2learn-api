ea5f01c4dc791e4cd700de2821fa8a72e9596475
dbauns <danielabaunsgaard@gmail.com>
2026-06-26 11:47:23 -0300

    Update database.py

--- database.py
+++ database.py
@@ -7,7 +7,9 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 print("DATABASE_URL =", repr(DATABASE_URL))
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL.replace("postgresql://", "postgresql+psycopg://")
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
